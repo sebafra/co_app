f553d1af13b0906d84040d1b6f4fe91a90dc3e48
Seba Fraresso <seba@diproach.com>
2014-02-14 17:28:06 -0300

    Delivery Look & Feel

--- app/Constants.js
+++ app/Constants.js
@@ -1,7 +1,7 @@
 window.Constants = {
-		//URL_BASE:"http://192.168.0.101:8080/consorciomovil/api",
-		URL_BASE:"http://200.58.118.223:8080/consorciomovil/api",
-		
+		URL_BASE:"http://192.168.1.8:8080/consorciomovil/api",
+		//URL_BASE:"http://200.58.118.223:8080/consorciomovil/api",
+
 		ROLE_USER:"user",
 		ROLE_ADMINISTRATOR:"administrator",
 
@@ -10,20 +10,20 @@ window.Constants = {
 
 		MESSAGE_ANSWERED_YES:"SI",
 		MESSAGE_ANSWERED_NO:"NO",
-		
+
 		MESSAGE_TYPE_ID_MESSAGE:"1",
 		MESSAGE_TYPE_ID_AUTHORIZATION:"2",
 		MESSAGE_TYPE_ID_BOOKING:"3",
 
-		
+
 		JSON_RESPONSE_STATUS_OK: "ok",
 		JSON_RESPONSE_STATUS_NOK: "nok",
-		
+
 		MESSAGE_STATE_PENDING	:"Pendiente",
 		MESSAGE_STATE_AUTHORIZED:"Autorizado",
 		MESSAGE_STATE_DENIED	:"No autorizado",
 
 		LOGIN_ERROR_MESSAGE_GENERIC: "No puede ingresar. Por favor intentar mas tarde.",
 		AJAX_JSON_ERROR_MESSAGE_GENERIC: "Error de comunicacion. Por favor intentar mas tarde."
-			
+
 };
